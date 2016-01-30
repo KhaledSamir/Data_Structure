971964ddad2448c04b5b7180bc213e8f9b5a01c6
KhaledSamir <khaledacmilan@gmail.com>
2016-01-30 07:27:14 -0600

    Unique String implementation

---
+++ DataS_Training/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataS_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Alia";
+            Console.WriteLine(uniquestring(x));
+           // Console.WriteLine('c' - 'a' + 1);
+        }
+
+        static bool uniquestring(string str)
+        {
+            if (str.Length > 256) return false;
+
+            bool[] chars = new bool[256];
+            for (int i = 0; i < str.Length; i++)
+            {
+                int val = str[i];
+                
+                if (chars[val])
+                    return false;
+
+                chars[val] = true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DataS_Training/QueueQuestions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataS_Training
+{
+    public class QueueQuestions
+    {
+        // implement queue by two stacks
+    }
+
+
+    public class QueueByTwoST
+    {
+        private Stack<string> stk1;
+
+        private Stack<string> stk2;
+
+
+    }
+}
